d54927666c558dcf45d4c7d77e6b7156807987a8
Virthe <virthe@accursed-lands.net>
2022-07-12 14:40:25 +0100

    Add eslint and prettier

--- leaflet.js
+++ leaflet.js
@@ -1,25 +1,24 @@
-const attributionStr =
-  '&copy; <a href="https://openstreetmap.org/copyright">' +
-  'OpenStreetMap contributors</a> | ' +
-  '<a href="https://www.wikidata.org" >Wikidata</a> | ' +
-  '<a href="https://github.com/xbgbtx/ArtistMap" >Source Code</a>';
-
 function initLeaflet() {
   const homeCoords = [0, 0];
   const homeZoom = 2;
+
+  const attributionStr =
+    '&copy; <a href="https://openstreetmap.org/copyright">' +
+    "OpenStreetMap contributors</a> | " +
+    '<a href="https://www.wikidata.org" >Wikidata</a> | ' +
+    '<a href="https://github.com/xbgbtx/ArtistMap" >Source Code</a>';
 
   const mapDiv = document.getElementById("map");
   const map = L.map(mapDiv).setView(homeCoords, homeZoom);
 
   // Add OpenStreetMap tiles
-  L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+  L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
     maxZoom: 19,
     attribution: attributionStr,
     noWrap: false,
   }).addTo(map);
 
-  /* L.easyButton('<img class="button_icon" src="assets/globe.svg">', () => { */
-  /*   map.flyTo(homeCoords, homeZoom); */
-  /* }).addTo(map); */
+  L.easyButton('<img class="button_icon" src="assets/globe.svg">', () => {
+    map.flyTo(homeCoords, homeZoom);
+  }).addTo(map);
 }
-
